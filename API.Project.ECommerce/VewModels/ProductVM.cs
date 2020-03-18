526c6b1f3fe2459d010ba5fd5048424c3b3f4bc2
kağan sütçü <kagansutcu@outlook.com>
2020-03-18 21:14:47 +0300

    Barkın için Init Class'ı açıldı.

---
+++ API.Project.ECommerce/VewModels/ProductVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Project.ECommerce.VewModels
+{
+    public class ProductVM
+    {
+    }
+}
